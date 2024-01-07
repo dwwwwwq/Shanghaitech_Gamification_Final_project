659c768671fca44a97aa144b8602eb4eeb2c5784
dwwwwwq <dwq747383@gmail.com>
2024-01-08 01:09:57 +0800

    关卡1

---
+++ Assets/scripts/sceneSwitcher1.cs
@@ -0,0 +1,22 @@
+namespace level1 {
+    
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher1 : MonoBehaviour
+{
+    public string nextSceneName = "level2"; // 指定要切换到的下一个场景
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // 当玩家进入触发区域时，触发场景切换事件
+            SceneManager.LoadScene(nextSceneName); // 直接加载场景
+            // EventManager.Instance.TriggerSceneSwitchCountEvent();
+        }
+    }
+}
+}
